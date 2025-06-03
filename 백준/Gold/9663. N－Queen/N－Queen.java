84de6bc11703a1d51082f8c061986f5fd0276dad
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-03 15:13:22 +0900

    [Gold IV] Title: N-Queen, Time: 2412 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,36 +1,37 @@
 import java.io.*;
 import java.util.*;
 
-class Main{	
-	public static int backtracking(int N, int row, boolean[] col, boolean[] dial_up, boolean[] dial_down) {
-		if(row==N) {
-			return 1;
+class Main {
+	static int cnt = 0;
+	static int N;
+
+	public static void backtracking(int row, boolean[] col, boolean[] dial_up, boolean[] dial_down) {
+		if (row == N) {
+			cnt++;
+			return;
 		}
-		int cnt = 0;
-		for(int c=0; c<N; c++) {
-			if(col[c] || dial_up[row+c] || dial_down[row-c+N-1])
+
+		for (int c= 0; c<N; c++) {
+			if (col[c] || dial_up[row+c] || dial_down[row-c+N-1]) {
 				continue;
+			}
 			col[c] = true;
 			dial_up[row+c] = true;
 			dial_down[row-c+N-1] = true;
-			cnt+=backtracking(N, row+1, col, dial_up, dial_down);
+			backtracking(row + 1, col, dial_up, dial_down);
 			col[c] = false;
-			dial_up[c+row] = false;
+			dial_up[row+c] = false;
 			dial_down[row-c+N-1] = false;
 		}
-		return cnt;
 	}
 
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		boolean[] col = new boolean[N];
-		boolean[] dial_up = new boolean[2*N-1];
-		boolean[] dial_down = new boolean[2*N-1];
-		
-		System.out.print(backtracking(N,0,col, dial_up, dial_down));
-		
+		boolean dial_up[] = new boolean[2*N-1];
+		boolean dial_down[] = new boolean[2*N-1];
+		backtracking(0, col, dial_up, dial_down);
+		System.out.println(cnt);
 	}
 }
