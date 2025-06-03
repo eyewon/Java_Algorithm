1446c279b28a6458ad91a18fc99897d4216c8718
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-03 15:46:19 +0900

    [Gold IV] Title: 스도쿠, Time: 444 ms, Memory: 24568 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,69 +1,71 @@
 import java.io.*;
 import java.util.*;
 
-class Main{	
-	static int[][] sdoku = new int[9][9];
+class Main {
+	static int[][] board =new int[9][9];
+	static int N;
 
-	
 	public static void backtracking(int row, int col) {
-
+		
+		if (row == 9) {
+			printBoard();
+			System.exit(0);
+		}
 		if(col == 9) {
 			backtracking(row+1, 0);
 			return;
 		}
-			
-		if(row == 9) {
-			StringBuilder sb = new StringBuilder();
-			for(int i=0; i<9; i++) {
-				for(int j=0; j<9; j++) {
-					sb.append(sdoku[i][j]).append(" ");
-				}
-				sb.append("\n");
-			}
-			System.out.print(sb);
-			System.exit(0); // 해 찾자마자 프로그램 종료
-		}
-		
-		if(sdoku[row][col] != 0)
+		if(board[row][col] != 0)
 			backtracking(row, col+1);
-		else {
-			for(int num = 1; num <= 9; num++) {
-				if(canPut(row, col, num)) {
-					sdoku[row][col] = num;
-					backtracking(row, col+1);
-					sdoku[row][col] = 0;
-				}
+		else{
+         for(int num=1; num<=9; num++) {
+			if(canPut(num, row, col)) {
+				board[row][col] = num;
+				backtracking(row, col+1);
+				board[row][col] = 0;
 			}
-		}
+		}   
+            
+        }
 	}
 	
-	public static boolean canPut(int row, int col, int num) {
+	public static boolean canPut(int num, int row, int col) {
 		for(int i=0; i<9; i++) {
-			if(sdoku[row][i]==num || sdoku[i][col]==num) 
-				return false;
+			if(board[row][i] == num) return false;
+			if(board[i][col] == num) return false;
 		}
-		int startRow = (row / 3) * 3;
-		int startCol = (col / 3) * 3;
 		
-		for(int i=startRow; i<startRow+3; i++) {
-			for(int j=startCol; j<startCol+3; j++) {
-				if(sdoku[i][j] == num)
-					return false;
+		int startX = (row/3) *3;
+		int startY = (col/3) *3;
+		
+		for(int i=startX; i<startX+3; i++) {
+			for(int j=startY; j<startY+3; j++) {
+				if(board[i][j] == num) return false;
 			}
 		}
+		
 		return true;
 	}
 	
+	public static void printBoard() {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<9; j++) {
+				sb.append(board[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for(int i=0; i<9; i++) {
+		for(int i=0; i<9; i++){
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0; j<9; j++) {
-				int input = Integer.parseInt(st.nextToken());
-				sdoku[i][j]=input;
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		backtracking(0,0);
-		
 	}
 }
