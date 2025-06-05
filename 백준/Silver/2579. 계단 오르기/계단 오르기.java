3eb463bdc8158acab48c3801409788d3790ba019
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-06 00:01:14 +0900

    [Silver III] Title: 계단 오르기, Time: 96 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -12,17 +12,18 @@ class Main {
 		for(int i=0; i<N; i++) {
 			score[i] = Integer.parseInt(br.readLine());
 		}
+		
 		dp[0] = score[0];
-if (N > 1) {
-    dp[1] = score[0] + score[1];
-}
-if (N > 2) {
-    dp[2] = Math.max(score[0] + score[2], score[1] + score[2]);
-
-    for (int i = 3; i < N; i++) {
-        dp[i] = Math.max(dp[i - 2] + score[i], dp[i - 3] + score[i - 1] + score[i]);
-    }
-}
-System.out.println(dp[N - 1]);
+		if(N>1) {
+			dp[1] = score[0] + score[1];
+			if(N>2) {
+				dp[2] = Math.max(score[2]+score[0], score[1]+score[2]);
+				for(int i=3; i<N; i++) {
+					dp[i] = Math.max(dp[i-2]+score[i], dp[i-3]+score[i-1]+score[i]);
+				}
+			}
+			
+		}
+		System.out.println(dp[N-1]);
  	}
 }
