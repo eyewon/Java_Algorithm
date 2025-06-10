7d03cc7eb4e5eabf737ce6c4e18d089a1fbb01db
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-11 03:22:51 +0900

    [Silver II] Title: 종이의 개수, Time: 932 ms, Memory: 309588 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static int cnt1 = 0;
+	static int cnt0 = 0;
+	static int cntM = 0;
+	
+	public static void cutPaper(int[][] arr, int x, int y, int N) {
+		if(canCut(arr, x, y, N)) {
+			int temp = arr[x][y];
+			if(temp==1) cnt1++;
+			else if(temp==0) cnt0++;
+			else cntM++;
+			return;
+		}
+		
+		int n = N/3;
+		
+		cutPaper(arr, x, y, n);
+		cutPaper(arr, x, y+n, n);
+		cutPaper(arr, x, y+2*n, n);
+		cutPaper(arr, x+n, y+n, n);
+		cutPaper(arr, x+n, y+2*n, n);
+		cutPaper(arr, x+n, y, n);
+		cutPaper(arr, x+2*n, y, n);
+		cutPaper(arr, x+2*n, y+n, n);
+		cutPaper(arr, x+2*n, y+2*n, n);
+	}
+	
+	public static boolean canCut(int[][] arr, int x, int y, int N) {
+		int cur = arr[x][y];
+		for(int i=x; i<x+N; i++) {
+			for(int j=y; j<y+N; j++) {
+				if(arr[i][j]!=cur)
+					return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] paper = new int[N][N];
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				paper[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		cutPaper(paper, 0, 0, N);
+		System.out.println(cntM);
+		System.out.println(cnt0);
+		System.out.println(cnt1);
+	}
+}
