ac032482e47f44b2b99a212d67b3586d8cf3410b
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-11 15:37:35 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -2,12 +2,15 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
 	public static long pow(long a, long b, long c) {
-		if(b == 0) return 1;
-		if(b%2==0) return pow(a*a % c, b/2, c);
-		else return a * pow(a, b-1, c) % c;
+		if(b==0)
+			return 1;
+		else if(b%2 == 0) {
+			return pow(a*a % c, b/2, c) % c;
+		}else
+			return a*pow(a, b-1, c) % c;
 	}
+
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
