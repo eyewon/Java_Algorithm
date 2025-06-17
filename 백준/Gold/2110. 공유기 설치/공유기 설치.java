e5afe34ad246091b1631544b46d50daf2e8b5e9a
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-17 15:54:16 +0900

    [Gold IV] Title: 공유기 설치, Time: 268 ms, Memory: 29380 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,44 +2,47 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static int C;
-	static int N;
-	static int[] x;
-	public static boolean isPossible(int dis) {
-		int cnt=1; //x[0]에 먼저 설치했다고 가정
-		int cur = x[0];
-		for(int i=1; i<N; i++) {
-			if(x[i]-cur >= dis) {
+
+	//가장 인접한 두 공유기 사이의 거리가 dist이도록 공유기를 설치했을 때, 설치된 공유기 개수
+	public static int cntRouter(int dist, int[] house) {
+		int cnt = 1;
+		int cur = house[0];
+		
+		for(int i=1; i<house.length; i++) {
+			if(house[i]-cur >= dist) {
 				cnt++;
-				cur = x[i];
+				cur = house[i];
 			}
 		}
-		return cnt>=C; //
+		return cnt;
 	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		x = new int[N];
-		for(int i=0; i<N; i++) {
-			x[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(x);
-		
-		int left = 1;
-		int right = x[N-1] - x[0];
-		int result = 0;
+	
+	//c개의 공유기 설치 가능한 최소 인접 거리 중 최댓값 (=upper bound)
+	public static int upperSearch(int left, int right, int target, int[] house) {
 		while(left <= right) {
-			int mid = left + (right-left)/2 ;
-			if(isPossible(mid)) {
-				result = mid;
+			int mid = left + (right - left)/2;
+			int installed = cntRouter(mid, house);
+			
+			if(installed >= target) {
 				left = mid+1;
 			}else {
 				right = mid-1;
 			}
 		}
-		System.out.println(result);
+		return left-1;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int[] house = new int[N];
+		for(int i=0; i<N; i++) {
+			house[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(house);
+		
+		System.out.println(upperSearch(1, house[N-1]-house[0], C, house));
 	}
 }
