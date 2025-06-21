e3557b43d110f9a95053579c1e4fd29c48d0c1cc
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-21 17:21:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static int N;
+	static int map[][];
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static boolean[][] visited;
+	static int cnt;
+
+	public static void dfs2D(int x, int y) {
+		visited[x][y] = true;
+		cnt++;
+		
+		for(int i=0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx > 0 && nx <= N && ny > 0 && ny <=N) {
+				if(!visited[nx][ny] && map[nx][ny] == 1) {
+					dfs2D(nx, ny);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N+1][N+1];
+		visited = new boolean[N+1][N+1];
+		
+		for(int i=1; i<=N; i++) {
+			String[] st = br.readLine().split("");
+			for(int j=1; j<=N; j++) {
+				map[i][j] = Integer.parseInt(st[j-1]);
+			}
+		}
+		
+		int totalCnt = 0;
+		ArrayList<Integer> cntList = new ArrayList<>();
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if(!visited[i][j] && map[i][j]==1) {
+					// 새로운 단지 발견
+					cnt = 0;
+					dfs2D(i,j);
+					cntList.add(cnt);
+					totalCnt ++;
+				}
+			}
+		}
+		Collections.sort(cntList);
+		StringBuilder sb = new StringBuilder();
+		sb.append(totalCnt).append("\n");
+		for(int i=0; i<totalCnt; i++) {
+			sb.append(cntList.get(i)).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
