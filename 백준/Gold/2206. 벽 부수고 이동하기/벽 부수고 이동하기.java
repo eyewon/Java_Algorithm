5e26174ba63557999059c6ee8225850143eb0719
eyewon <111197714+eyewon@users.noreply.github.com>
2025-06-23 13:51:47 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 828 ms, Memory: 161604 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -4,7 +4,6 @@ import java.util.*;
 class Main {
 	static int N, M;
 	static int[][] map;
-	static boolean[][][] visited; // (x, y, 벽을 부쉈는지 여부)
 	static int[][][] dist;
 	static int[] dx = {0, 0, -1, 1};
 	static int[] dy = {1, -1, 0, 0};
@@ -13,7 +12,6 @@ class Main {
 		Queue<int[]> queue = new LinkedList<>();
 		
 		queue.add(new int[] {1,1,0});
-		visited[1][1][0] = true;
 		dist[1][1][0] = 1;
 		
 		while(!queue.isEmpty()) {
@@ -29,13 +27,11 @@ class Main {
 				if(nx>0 && nx<=N && ny>0 && ny<=M) {
 					if(map[nx][ny] == 1 && cw == 1) continue;
 					
-					if(map[nx][ny] == 1 && cw == 0 && !visited[nx][ny][1]){
+					if(map[nx][ny] == 1 && dist[nx][ny][0]==0){
 						queue.add(new int[] {nx, ny, 1});
-						visited[nx][ny][1] = true;
 						dist[nx][ny][1] = dist[cx][cy][0] + 1;
-					} else if(map[nx][ny] == 0 && !visited[nx][ny][cw]) {
+					} else if(map[nx][ny] == 0 && dist[nx][ny][cw]==0) {
 						queue.add(new int[] {nx, ny, cw});
-						visited[nx][ny][cw] = true;
 						dist[nx][ny][cw] = dist[cx][cy][cw] + 1;
 					}
 				}
@@ -50,13 +46,12 @@ class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N+1][M+1];
-		visited = new boolean[N+1][M+1][2];
 		dist = new int[N+1][M+1][2];
 		
 		for(int i=1; i<=N; i++) {
-			String[] input = br.readLine().split("");
+			String input = br.readLine();
 			for(int j=0; j<M; j++) {
-				map[i][j+1] = Integer.parseInt(input[j]);
+				map[i][j+1] = input.charAt(j)-'0';
 			}
 		}
 		bfs();
@@ -64,5 +59,6 @@ class Main {
 		if(dist[N][M][0] !=0 ) result = dist[N][M][0];
 		if(dist[N][M][1] != 0) result = (result == -1) ? dist[N][M][1] : Math.min(result, dist[N][M][1]);
 		System.out.println(result);
+
 	}
 }
