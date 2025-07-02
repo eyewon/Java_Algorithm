f77d61e3f3f90d1823f3905ab4aac8a6f8e8b09e
eyewon <111197714+eyewon@users.noreply.github.com>
2025-07-02 20:18:39 +0900

    [Gold V] Title: 두 용액, Time: 348 ms, Memory: 28548 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -5,33 +5,37 @@ class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int N = Integer.parseInt(br.readLine());
 		
 		int[] arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr);
 
-		int left=0, right = N-1;
-		int min = Integer.MAX_VALUE;
-		int ans1=0, ans2=0;
-		while(left < right) {
-			int sum = arr[left] + arr[right];
-			if(Math.abs(sum) < Math.abs(min)) {
-				min = sum;
-				ans1 = arr[left];
-				ans2 = arr[right];
-			}else if(sum < 0) {
-				left++;
-			}else if(sum > 0){
-				right--;
-			}else if(sum == 0) {
+		int start = 0, end = N-1, ans1 = 0, ans2 = 0;
+		int minSum = Integer.MAX_VALUE;
+		
+		while(start < end) {
+			int sum = arr[start] + arr[end];
+			if(Math.abs(sum) < Math.abs(minSum)) {
+				minSum = sum;
+				ans1 = arr[start];
+				ans2 = arr[end];
+			}
+			if(sum < 0) {
+				start++;
+			}else if(sum > 0) {
+				end--;
+			}else {
+				ans1 = arr[start];
+				ans2 = arr[end];
 				break;
 			}
 		}
-		System.out.println(ans1 + " "+ ans2);
+		
+		System.out.println(ans1 + " " + ans2);
 	}
 }
