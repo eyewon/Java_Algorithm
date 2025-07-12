a331c837d016fa9d14b85961c3775f31fef9b1cd
eyewon <111197714+eyewon@users.noreply.github.com>
2025-07-12 15:19:26 +0900

    [Gold II] Title: 트리의 지름, Time: 648 ms, Memory: 98128 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -4,20 +4,19 @@ import java.util.*;
 class Main {
 	static List<Node>[] tree;
 	static boolean[] visited;
-	static long maxDist;
-	static int farthestNode = 0;
+	static int maxDist;
+	static int farthestNode;
 	
 	static class Node{
-		int to;
-		int cost;
+		int to, cost;
 		
 		Node(int to, int cost){
 			this.to = to;
 			this.cost = cost;
 		}
 	}
-	
-	public static void dfs(int node, long dist) {
+
+	public static void dfs(int node, int dist) {
 		visited[node] = true;
 		
 		if(dist > maxDist) {
@@ -27,7 +26,7 @@ class Main {
 		
 		for(Node next: tree[node]) {
 			if(!visited[next.to]) {
-				dfs(next.to, dist+ next.cost);
+				dfs(next.to, dist+next.cost);
 			}
 		}
 	}
@@ -36,31 +35,31 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int V = Integer.parseInt(br.readLine());
-		
 		tree = new ArrayList[V+1];
 		
 		for(int i=1; i<=V; i++) tree[i] = new ArrayList<>();
 		
 		for(int i=1; i<=V; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
 			int from = Integer.parseInt(st.nextToken());
+			
 			while(true) {
 				int to = Integer.parseInt(st.nextToken());
-				if(to==-1) break;
+				if(to == -1) break;
 				int cost = Integer.parseInt(st.nextToken());
+				
 				tree[from].add(new Node(to, cost));
 			}
 		}
 		
 		visited = new boolean[V+1];
 		maxDist = 0;
-		dfs(1, 0); //아무노드에서 시작해서 가장 먼 노드 찾기
-		int A = farthestNode;
+		farthestNode = 0;
+		dfs(1,0);
 		
 		visited = new boolean[V+1];
 		maxDist = 0;
-		dfs(A, 0); //A에서 가장 먼 노드 B 찾기 -> 이거리 = 트리의 지름
+		dfs(farthestNode, 0);
 		
 		System.out.println(maxDist);
 	}
