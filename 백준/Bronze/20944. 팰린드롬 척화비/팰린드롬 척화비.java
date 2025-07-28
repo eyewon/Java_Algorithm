de7e0a24c813a463385ea737472bea0b52e5492c
eyewon <111197714+eyewon@users.noreply.github.com>
2025-07-28 19:51:29 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 192 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 항상 같은 문자 반복하면 수미상관 + 팰린드롬이 됨 (ex. aaaa...)
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append('a');  // a, b, c 등 소문자 중 아무거나 가능
-        }
-
-        System.out.println(sb.toString());
-    }
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<N; i++) sb.append("a");
+		System.out.println(sb);
+	}
 }
